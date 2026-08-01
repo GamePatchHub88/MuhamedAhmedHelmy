f3fbf635b6b791c9814015b0aa17175d6026f7e8
Mohamed Ahmed Helmy Aboura <mh3976698@gmail.com>
2026-08-01 23:49:34 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -13,7 +13,7 @@
 // وحطه هنا بدل القيمة الفاضية.
 
 const SUPABASE_URL = "https://nethlnekztwurphnqtvl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldGhsbmVrenR3dXJwaG5xdHZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzY4MjEzOCwiZXhwIjoyMDg5MjU4MTM4fQ.UQYcBzDPPdJYvawL-tfBmRQIgOQMmbl63atzvsszXBs";
+const SUPABASE_ANON_KEY = "PUT_YOUR_ANON_PUBLIC_KEY_HERE";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,

d52991baea46938baf01b6bf95dec9d58d1d8eae
Mohamed Ahmed Helmy Aboura <mh3976698@gmail.com>
2026-08-21 10:42:29 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 
 
-const SUPABASE_URL = "https://nethlnekztwurphnqtvl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldGhsbmVrenR3dXJwaG5xdHZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzY4MjEzOCwiZXhwIjoyMDg5MjU4MTM4fQ.UQYcBzDPPdJYvawL-tfBmRQIgOQMmbl63atzvsszXBs";
+const SUPABASE_URL = "https://wgcfajrwfmunwoqmneww.supabase.co/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnY2ZhanJ3Zm11bndvcW1uZXd3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzM0ODc4NCwiZXhwIjoyMDk4OTI0Nzg0fQ.oC9Z9XI93TynwYePXApfdewt4kXMlyZ3JlteVf_I-7U";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,

cee73bb75f82f4e26e7f85d731d2ea965411a9a1
Mohamed Ahmed Helmy Aboura <mh3976698@gmail.com>
2026-08-21 10:51:14 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 
 
 const SUPABASE_URL = "https://wgcfajrwfmunwoqmneww.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnY2ZhanJ3Zm11bndvcW1uZXd3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzM0ODc4NCwiZXhwIjoyMDk4OTI0Nzg0fQ.oC9Z9XI93TynwYePXApfdewt4kXMlyZ3JlteVf_I-7U";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnY2ZhanJ3Zm11bndvcW1uZXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNDg3ODQsImV4cCI6MjA5ODkyNDc4NH0.sR7YNvM3YKGQkavCN617r9B30oEwf06dCuuRz83ojlE";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
